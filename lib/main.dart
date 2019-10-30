e863a92a7911450983bd53948943dbb8d0e0e1fb
NaturalGao <924789862@qq.com>
2019-10-30 20:25:24 +0800

    fix: 更新底部菜单

--- lib/main.dart
+++ lib/main.dart
@@ -25,45 +25,25 @@ class Home extends StatelessWidget {
     return DefaultTabController(
         length: 3,
         child: Scaffold(
-          backgroundColor: Colors.grey[100],
-          appBar: AppBar(
-              title: Text('FLUTTER DEMO'),
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.search),
-                  tooltip: 'Navigration',
-                  onPressed: () => debugPrint('Search button is pressed.'),
-                )
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+                title: Text('FLUTTER DEMO'),
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    tooltip: 'Navigration',
+                    onPressed: () => debugPrint('Search button is pressed.'),
+                  )
+                ],
+                elevation: 30.0),
+            body: TabBarView(
+              children: <Widget>[
+                ListViewDemo(),
+                Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+                Icon(Icons.directions_bike, size: 128.0, color: Colors.black12)
               ],
-              elevation: 30.0),
-          body: TabBarView(
-            children: <Widget>[
-              ListViewDemo(),
-              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12)
-            ],
-          ),
-          drawer: DrawerDemo(),
-          bottomNavigationBar: TabBar(
-            unselectedLabelColor: Colors.black38,
-            indicatorColor: Colors.black54,
-            indicatorSize: TabBarIndicatorSize.label,
-            indicatorWeight: 1.0,
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.home),
-                text: '首页',
-              ),
-              Tab(
-                icon: Icon(Icons.dashboard),
-                text: '分类',
-              ),
-              Tab(
-                icon: Icon(Icons.person),
-                text: '我的',
-              ),
-            ],
-          ),
-        ));
+            ),
+            drawer: DrawerDemo(),
+            bottomNavigationBar: BottomNavigationBarDemo()));
   }
 }
